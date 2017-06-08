d42b1857a51bc82cd915ea0d169c1d76b14ff87b
tjaks <tommy@jaksdesign.se>
2017-06-08 10:26:11 +0200

    Comment about mapping id in object

--- webb/src/reducers/reducer_posts.js
+++ webb/src/reducers/reducer_posts.js
@@ -1,7 +1,12 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 
-//map id 
+/* 
+    Converts initial state to a object and using lodash to map the
+    id to from the action.payload so it will be easier to get data.
+
+    Instead of using loops, we can now just use state["id"].
+*/
 
 export default function (state = {}, action) {
     switch (action.type) {
